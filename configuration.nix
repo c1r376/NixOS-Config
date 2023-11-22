1d7fca72059fe01610c1789f833c800de69aca4e
c1r376 <137849551+c1r376@users.noreply.github.com>
2023-11-22 09:19:03 +0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,61 +2,21 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }: let
-  nix-gaming = import (builtins.fetchTarball "https://github.com/fufexan/nix-gaming/archive/master.tar.gz");
-in
+{ config, pkgs, ... }:
 
 {
-  
-  programs.hyprland.enable = true;
-
-  # 1. enable vaapi on OS-level
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  };
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-    ];
-  };
-
-  # 2. do not forget to enable jellyfin
-  services.jellyfin.enable = true;
-  services.blueman.enable = true;
-
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.settings = {
-    General = {
-      Enable = "Source,Sink,Media,Socket";
-    };
-  };
-
-  #hardware.opengl.driSupport32bit = true;
-  #nixpkgs.config.allowUnfreePredicate = (pkg: builtins.elem (builtins.parseDrvName pkg.name).name [ "steam" ]);
-  #nix.settings = {
-  #   substituters = [https://nix-gaming.cachix.org"];
-  #   trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAx1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
-  #  };
-
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-
-    "${nix-gaming}/modules/pipewireLowLatency.nix"   
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
-  boot.supportedFilesystems = [ "ntfs" ];
+	boot.kernelPackages = pkgs.linuxPackages_xanmod;
+	boot.supportedFilesystems = [ "ntfs"];
 
-  networking.hostName = "DellCurry"; # Define your hostname.
+  networking.hostName = "x260"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -67,142 +27,186 @@ in
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Asia/Singapore";
+  time.timeZone = "America/Chicago";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_SG.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_SG.UTF-8";
-    LC_IDENTIFICATION = "en_SG.UTF-8";
-    LC_MEASUREMENT = "en_SG.UTF-8";
-    LC_MONETARY = "en_SG.UTF-8";
-    LC_NAME = "en_SG.UTF-8";
-    LC_NUMERIC = "en_SG.UTF-8";
-    LC_PAPER = "en_SG.UTF-8";
-    LC_TELEPHONE = "en_SG.UTF-8";
-    LC_TIME = "en_SG.UTF-8";
+    LC_ADDRESS = "es_US.UTF-8";
+    LC_IDENTIFICATION = "es_US.UTF-8";
+    LC_MEASUREMENT = "es_US.UTF-8";
+    LC_MONETARY = "es_US.UTF-8";
+    LC_NAME = "es_US.UTF-8";
+    LC_NUMERIC = "es_US.UTF-8";
+    LC_PAPER = "es_US.UTF-8";
+    LC_TELEPHONE = "es_US.UTF-8";
+    LC_TIME = "es_US.UTF-8";
   };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
+	enable = true;
+	displayManager.sddm = {
+		enable = true;
+		theme = "maya";
+		autoNumlock = true;
+	};
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+	programs.thunar.plugins =
+	with pkgs.xfce;
+	[ thunar-archive-plugin ]
+	;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+	programs.hyprland = {
+		enable = true;
+		xwayland.hidpi = true;
+		xwayland.enable = true;
+	};
 
-    lowLatency = {
-      # enable this module
-      enable = true;
-      #defaults (no need to be set unless modified)
-      quantum = 64;
-      rate = 48000;
-    };
+	# Hint Electron apps to use wayland
+	environment.sessionVariables = {
+		NIXOS_OZONE_WL = "1";
+	};
 
+	services.dbus.enable = true;
+	xdg.portal = {
+		enable = true;
+		wlr.enable = true;
+		extraPortals = [
+			pkgs.xdg-desktop-portal-gtk
+		];
+	};
 
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+	nixpkgs.overlays = [
+		(self: super: {
+		waybar = super.waybar.overrideAttrs (oldAttrs: {
+			mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+		});
+		})
+	];
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+	fonts.fonts = with pkgs; [
+		nerdfonts
+		meslo-lgs-nf
+		font-awesome
+		google-fonts
+	];
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+	services.blueman.enable = true;
+
+	sound.enable = true;
+		hardware = {
+			bluetooth.enable = true;
+		};
+
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+		jack.enable = true;
+	};
+
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.c1r3 = {
     isNormalUser = true;
     description = "c1r3";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-      kate
-      # thunar
-    ];
+    packages = with pkgs; [];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+	nixpkgs.config.allowInsecure = true;
+	nixpkgs.config.permittedInsecurePackages = [
+	"openssl-1.1.1w"
+	];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    antimicrox
-    audacity
-    blender
-    bookworm
-    btop
-    cargo
-    cava
-    celluloid
-    eww
-    eww-wayland
-    gimp
-    git
-    gparted
-    handbrake
-    inkscape
-    jellyfin
-    jellyfin-web
-    kdenlive
-    kitty
-    kodi
-    libreoffice
-    lutris
-    mako
-    mpv
-    neofetch
-    opera
-    pamixer
-    polkit-kde-agent
-    protonup-ng
-    protonup-qt
-    qbittorrent
-    rustc
-    spotify
-    steam
-    steam-run
-    swaybg
-    swayidle
-    swayimg
-    swaylock-effects
-    # swwww
-    # thunar
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    wine
-    wlogout
-    wlroots
-    (lutris.override {
-           extraPkgs = pkgs: [
-             # List package dependencies here
-             wineWowPackages.stable
-             winetricks
-           ];
-         })
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+	hyprland
+	swww # for wallpapers
+	xdg-desktop-portal-gtk
+	xdg-desktop-portal-hyprland
+	xwayland
+	greetd.wlgreet
+	polkit
 
-    #nix-gaming.packages.${pkgs.hostPlatform.system}.<package>
+	meson
+	wayland-protocols
+	wayland-utils
+	wl-clipboard
+	wlroots
+
+	rofi-wayland
+	wofi
+
+	blueman
+	brightnessctl
+	dunst
+	foot
+	git
+	kitty
+	libnotify
+	nano
+	pavucontrol
+	pipewire
+	wlr-randr
+
+	hyprpaper
+	swaybg
+	wpaperd
+	mpvpaper
+	swww
+	eww
+	eww-wayland
+	mako
+	waybar
+	
+	antimicrox
+	audacity
+	blender
+	btop
+	celluloid
+	firefox-wayland
+	gimp
+	github-desktop
+	google-chrome
+	gparted
+	handbrake
+	inkscape
+	kdenlive
+	kitty
+	libreoffice
+	kodi
+	libreoffice
+	lutris
+	neofetch
+	openssl
+	popcorntime
+	protonup-ng
+	protonup-qt
+	qbittorrent
+	ranger
+	steam
+	steam-run
+	sublime
+	unzip
+	wget
+	wine
+	xfce.thunar
+	zip
+
+
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -223,14 +227,10 @@ in
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.allowReboot = true;
-  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-unstable";
+	system.copySystemConfiguration = true;
+	system.autoUpgrade.enable = true;
+	system.autoUpgrade.allowReboot = true;
+	system.autoUpgrade.channel = "https://channels.nixos.org/nixos-unstable";
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -239,6 +239,5 @@ in
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-
 
 }
